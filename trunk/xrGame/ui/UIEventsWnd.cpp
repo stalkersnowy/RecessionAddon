528fc519a35b9d85629ffb9921adbefd34ef405a
Snowy <snowy.blackcat@gmail.com>
2025-05-13 17:48:44 +0700

    Обход проблемы с глобальной картой
    Если развернуть описание задания, переключиться на другой раздел ПДА и обратно и свернуть описание, то на глобальной карте не будут отображаться карты уровней - теперь описание будет само сворачиваться при смене раздела

--- trunk/xrGame/ui/UIEventsWnd.cpp
+++ trunk/xrGame/ui/UIEventsWnd.cpp
@@ -166,6 +166,8 @@ void CUIEventsWnd::ReloadList(bool bClearOnly)
 
 void CUIEventsWnd::Show(bool status)
 {
+	if (!GetDescriptionMode())
+		SetDescriptionMode(true);
 	inherited::Show			(status);
 	m_UIMapWnd->Show		(status);
 	m_UITaskInfoWnd->Show	(status);
