89bef6b45580d28705a132aa02009baacaebe946
Snowy <snowy.blackcat@gmail.com>
2025-10-04 06:59:18 +0700

    Мелкие правки
    Правка мелкой текстурной дыры в ангарах у бункера на Янтаре
    Внёс фикс лучей из SSS и в старые каскады, поправил фикс тумана у пуха
    Переделка фикса лифта из основы
    Фикс недосмотра при правках HW (походу из-за этого на стриме были графические артефакты, ы)

--- trunk/layers/xrRender_R2/r2.h
+++ trunk/layers/xrRender_R2/r2.h
@@ -127,7 +127,7 @@ public:
 	float														o_hemi			;
 	float														o_hemi_cube[CROS_impl::NUM_FACES]	;
 	float														o_sun			;
-	IDirect3DQuery9*											q_sync_point[2]	;
+	IDirect3DQuery9*											q_sync_point[CHWCaps::MAX_GPUS];
 	u32															q_sync_count	;
 
 	bool														m_bFirstFrameAfterReset;	// Determines weather the frame is the first after resetting device.
