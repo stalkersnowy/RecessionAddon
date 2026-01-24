7ba152162a69e272a4cf5d6d68cfa792d81f309c
Snowy <snowy.blackcat@gmail.com>
2026-01-24 21:57:10 +0700

    Правка UI из ЧН 3
    Ещё забытая правка

--- trunk/xrGame/UIDialogHolder.cpp
+++ trunk/xrGame/UIDialogHolder.cpp
@@ -221,7 +221,7 @@ void CDialogHolder::StartStopMenu(CUIDialogWnd* pDialog, bool bDoHideIndicators)
 		StopMenu(pDialog);
 	else
 	{
-		GetUICursor()->SetUICursorPosition	(Fvector2().set(512.0f,384.0f));
+		GetUICursor()->SetUICursorPosition(Fvector2().set(UI_BASE_WIDTH / 2.f, UI_BASE_HEIGHT / 2.f));
 		StartMenu(pDialog, bDoHideIndicators);
 	}
 	
